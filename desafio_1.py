27a92bc87cd489ea869287343449d3c599b01398
ConstanzaMorales <constanza.morales.a@usach.cl>
2022-08-23 00:07:12 -0400

    first commi

---
+++ desafio_1.py
@@ -0,0 +1,6 @@
+print('Hola, calculamos tu promedio')
+nota_1 = int(input('Ingresa tu nota 1: '))
+nota_2 = int(input('Ingresa tu nota 2: '))
+nota_3 = int(input('Ingresa tu nota 3: '))
+promedio = nota_1 * 0.2 + nota_2 * 0.3 + nota_3 * 0.5
+print("Tu promedio es: ", promedio)
